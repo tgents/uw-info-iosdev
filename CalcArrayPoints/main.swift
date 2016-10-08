70846b64ad445ee5f01cd8da7bdada366763b1cf
thomas <tgents@uw.edu>
2016-10-08 15:02:34 -0700

    first commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by iGuest on 10/8/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
