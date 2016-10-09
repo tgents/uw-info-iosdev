e282c592814d9c39caed3aa366ff170221b6c10b
thomas <tgents@uw.edu>
2016-10-08 17:40:18 -0700

    all unit tests passed

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -60,18 +60,43 @@ func add(p1: (Int, Int), p2: (Int, Int)) -> (Int, Int){
     return (add(left: p1.0, right: p2.0), add(left: p1.1, right: p2.1))
 }
 
-func substract(p1: (Int, Int), p2: (Int, Int)) -> (Int, Int){
+func subtract(p1: (Int, Int), p2: (Int, Int)) -> (Int, Int){
     return (subtract(left: p1.0, right: p2.0), subtract(left: p1.1, right: p2.1))
 }
 
-func add(p1: [String : Int], p2: [String : Int]) -> [String : Int]{
+func add(p1: [String : Int], p2: [String : Int]) -> [String : Int]?{
+    if p1["x"] == nil || p1["y"] == nil || p2["x"] == nil || p2["y"] == nil {
+        return nil
+    }
     return ["x": add(left: p1["x"]!, right: p2["x"]!), "y": add(left: p1["y"]!, right: p2["y"]!)]
 }
 
-func subtract(p1: [String : Int], p2: [String : Int]) -> [String : Int]{
+func subtract(p1: [String : Int], p2: [String : Int]) -> [String : Int]?{
+    if p1["x"] == nil || p1["y"] == nil || p2["x"] == nil || p2["y"] == nil {
+        return nil
+    }
     return ["x": subtract(left: p1["x"]!, right: p2["x"]!), "y": subtract(left: p1["y"]!, right: p2["y"]!)]
 }
 
+func add(p1: [String : Double]?, p2: [String : Double]?) -> [String : Double]?{
+    if p1 == nil || p2 == nil || p1!["x"] == nil || p1!["y"] == nil || p2!["x"] == nil || p2!["y"] == nil {
+        return nil
+    }
+    return ["x": p1!["x"]! + p2!["x"]!, "y": p1!["y"]! + p2!["y"]!]
+}
 
-print(reduce(array: [1,2,3,4,5], operation: average))
+func subtract(p1: [String : Double]?, p2: [String : Double]?) -> [String : Double]?{
+    if p1 == nil || p2 == nil || p1!["x"] == nil || p1!["y"] == nil || p2!["x"] == nil || p2!["y"] == nil {
+        return nil
+    }
+    return ["x": p1!["x"]! - p2!["x"]!, "y": p1!["y"]! - p2!["y"]!]
+}
+
+let nilEntryDict : Dictionary<String, Double>? = nil
+let dictionaryDoubleTest2 = [
+    "x" : 5.5,
+    "y" : 4.5
+]
+
+print(add(p1: nilEntryDict, p2: dictionaryDoubleTest2))
 
