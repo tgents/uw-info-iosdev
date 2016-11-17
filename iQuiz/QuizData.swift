13d16c44686902d8879d51a6d87834de4c842dac
Thomas <tgents@uw.edu>
2016-11-16 22:38:16 -0800

    updated data loading

--- iQuiz/QuizData.swift
+++ iQuiz/QuizData.swift
@@ -11,17 +11,22 @@ import UIKit
 class QuizData: NSObject {
     static let shared = QuizData()
     static let path = Bundle.main.path(forResource: "data", ofType: "txt")!
-    static let urlToRequest: String = "http://tednewardsandbox.site44.com/questions.json"
+    var urlToRequest: String = "http://tednewardsandbox.site44.com/questions.json"
     var data: [[String:Any]]?
     override init() {
+        super.init()
+        load()
+    }
+    
+    func load() {
         do{
-            var content = NSData(contentsOf: URL(string: QuizData.urlToRequest)!)
+            var content = NSData(contentsOf: URL(string: urlToRequest)!)
             if(content == nil){
                 content = NSData(contentsOfFile: QuizData.path)
             }
             if(content != nil){
                 data = try JSONSerialization.jsonObject(with: content as! Data, options: []) as! [[String:Any]]
-                QuizData.save(content!)
+                save(content!)
             }
             
         } catch {
@@ -29,7 +34,7 @@ class QuizData: NSObject {
         }
     }
     
-    static func save(_ content:NSData){
+    func save(_ content:NSData){
         do {
             if let JSONString = String(data: content as Data, encoding: String.Encoding.utf8) {
                 print(JSONString)
